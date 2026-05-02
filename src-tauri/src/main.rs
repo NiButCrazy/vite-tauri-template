4ba8f4c8b34b1561479a890456295f73564531e1
NiButCrazy <3104955349@qq.com>
2026-05-02 11:47:54 +0800

    feat: 添加了 pnpm 验证脚本

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,4 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// 防止在 Windows 发布版本中增加控制台窗口, 不要移除!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
